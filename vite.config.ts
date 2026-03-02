2e653bb7a919d7990500fa3c5ad58d726ffcc718
Dzangiev <mo.dzangi@gmail.com>
2026-03-02 22:23:20 +0300

    ci: add GitHub Actions workflow for deployments and fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Hadel-iz/',
   plugins: [
     react(),
     VitePWA({
@@ -29,7 +30,7 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any'  
+            purpose: 'any'
           },
           {
             src: 'maskable-icon-512x512.png',
